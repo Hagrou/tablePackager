53220239f6ae53f02c975bbb9e98c6e40b5fbb68
Hagrou <hagrou@gmail.com>
2021-04-22 21:55:02 +0200

    Add search + database

--- packager/tools/exception.py
+++ packager/tools/exception.py
@@ -1,3 +1,5 @@
+import traceback
+
 
 class PackageException(Exception):
     def __init__(self, msg):
@@ -7,6 +9,7 @@ class PackageException(Exception):
     def msg(self):
         return self.__msg
 
+
 class Directb2sException(Exception):
     def __init__(self, table):
         self.__table = table
@@ -14,6 +17,7 @@ class Directb2sException(Exception):
     @property
     def table(self):
         return self.__table
+
 
 class RomException(Exception):
     def __init__(self, table, rom):
@@ -27,3 +31,30 @@ class RomException(Exception):
     @property
     def rom(self):
         return self.__rom
+
+
+class ConnectionException(Exception):
+    def __init__(self, name: str = '', url: str = '', error_code: int = 0):
+        self.__name = name
+        self.__url = url
+        self.__error_code = error_code
+        self.__stack = ''.join(traceback.format_exception(self.__class__, self, self.__traceback__))
+
+    def __str__(self):
+        return 'Connection Error %s (%d)\n%s' % (self.__url, self.__error_code, self.__stack)
+
+    @property
+    def name(self):
+        return self.__name
+
+    @property
+    def url(self):
+        return self.__url
+
+    @property
+    def error_code(self):
+        return self.__error_code
+
+    @property
+    def stack(self):
+        return ''.join(traceback.format_exception(self.__class__, self, self.__traceback__))
