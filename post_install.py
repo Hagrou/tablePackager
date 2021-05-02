61d98c1520662c018ba054916ba19993f09da45a
Hagrou <hagrou@gmail.com>
2021-05-02 23:42:33 +0200

    + post_install and database

---
+++ post_install.py
@@ -0,0 +1,20 @@
+# post install script
+import os
+import shutil
+import sys
+
+if __name__ == "__main__":
+    args = sys.argv[1:]
+
+    user_dir=os.path.expanduser('~/tablePackager')
+    if not os.path.exists(user_dir):
+        os.makedirs(user_dir)
+    if not os.path.exists(user_dir+'/database'):
+        os.makedirs(user_dir+'/database')
+    shutil.copyfile('lib/packager/database/manufacturer.json',user_dir+'/database/manufacturer.json')
+
+    if os.path.exists(user_dir+'/database/pinball_machines.json'):
+        shutil.copyfile(user_dir+'/database/pinball_machines.json', user_dir+'/database/pinball_machines.json.backup')
+    shutil.copyfile('lib/packager/database/pinball_machines.json', user_dir + '/database/pinball_machines.json')
+
+    sys.exit(0)
--- setup.py
+++ setup.py
@@ -3,6 +3,9 @@ from cx_Freeze import setup, Executable
 import os.path
 import shutil
 
+#https://fraoustin.fr/old/python_cx_freeze.html
+#https://stackoverflow.com/questions/57184971/available-bdist-msi-options-when-creating-msi-with-cx-freeze
+
 from packager.tablePackager import version
 from packager.help.genHelp import *
 
@@ -26,26 +29,7 @@ shortcut_table = [
      )
     ]
 
-options = {
-    'build_exe': {
-        "excludes": ["numpy"], # cx_freeze 6.2 crash with numpy 1.19
-        'include_msvcr' : True,
-        'include_files':[
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
-            os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
-         ],
-    },
-    'bdist_msi': {'data': {'Shortcut': shortcut_table},
-                  'upgrade_code': '{006d3301-d595-49e5-81d0-4a906aa48bb8}' # required for msi upgrade
-                 },
-}
 
-executables = [
-    Executable('packager/tablePackager.py',
-               base='Win32GUI',
-               icon='packager/images/tablePackager_128x128.ico',
-               )
-]
 
 # force clean
 if os.path.exists('build'):
@@ -58,7 +42,21 @@ gen_about('./about.md','packager/help',version)
 setup(name='tablePackager',
       version=version,
       description='Pincab Table Packager',
-      options=options,
-      executables=executables
+      options={ 'build_exe': { 'include_msvcr' : True,
+                               'include_files':[
+                                    os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tk86t.dll'),
+                                    os.path.join(PYTHON_INSTALL_DIR, 'DLLs', 'tcl86t.dll'),
+                                'post_install.py'
+                              ]},
+                'bdist_msi': {'data': {'Shortcut': shortcut_table},
+                              'install_icon' : 'packager/images/tablePackager_128x128.ico',
+                              'upgrade_code': '{006d3301-d595-49e5-81d0-4a906aa48bb8}', # required for msi upgrade
+                             },
+      },
+      url='https://github.com/Hagrou/tablePackager',
+      license="GPL 3.0",
+      executables=[ Executable(script='packager/tablePackager.py',
+                               base='Win32GUI',
+                               icon='packager/images/tablePackager_128x128.ico')]
 )
 
